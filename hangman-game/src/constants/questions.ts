0a20cd9eacdad7a0d5f429087e04a376ff11855a
Olya Y <f_akora@mail.ru>
2024-01-27 22:57:49 +0200

    feat: comprise the list of question-answer pairs

---
+++ hangman-game/src/constants/questions.ts
@@ -0,0 +1,73 @@
+interface IQuestion {
+    question: string;
+    answer: string;
+}
+
+const questions: IQuestion[] = [
+    {
+        question: 'Which planet is known as the Red Planet?',
+        answer: 'Mars',
+    },
+    {
+        question: 'Who painted the Mona Lisa? Please indicate the first name.',
+        answer: 'Leonardo',
+    },
+    {
+        question: 'What is the largest organ in the human body?',
+        answer: 'Skin',
+    },
+    {
+        question: 'What is the capital of Japan?',
+        answer: 'Tokyo',
+    },
+    {
+        question: 'Who wrote Romeo and Juliet? Please indicate the last name.',
+        answer: 'Shakespeare',
+    },
+    {
+        question: 'How many colors are there in a rainbow?',
+        answer: 'Seven',
+    },
+    {
+        question: 'What is the capital of Australia?',
+        answer: 'Canberra',
+    },
+    {
+        question: 'What is the process by which plants make their own food?',
+        answer: 'Photosynthesis',
+    },
+    {
+        question: 'What is the national animal of Australia?',
+        answer: 'Kangaroo',
+    },
+    {
+        question: 'How many sides does an octagon have?',
+        answer: 'Eight',
+    },
+    {
+        question: 'Which element does "O" represent on the periodic table?',
+        answer: 'Oxygen',
+    },
+    {
+        question: 'What is the largest bird in the world?',
+        answer: 'Ostrich',
+    },
+    {
+        question: 'Which planet is known as the Morning Star and Evening Star?',
+        answer: 'Venus',
+    },
+    {
+        question: '140. Who is known as the Father of the Nation in India? Please indicate the last name.',
+        answer: 'Mahatma Gandhi',
+    },
+    {
+        question: 'Who was the famous scientist known for his theory of relativity? Please indicate the last name.',
+        answer: 'Albert Einstein',
+    },
+    {
+        question: 'Who is known as the Father of Medicine?',
+        answer: 'Hippocrates',
+    },
+]
+
+export default questions;
