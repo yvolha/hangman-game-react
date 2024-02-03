9fb33f6bc11bd729656e03c13d76ec3d62f421e9
Olya Y <f_akora@mail.ru>
2024-02-03 23:50:34 +0200

    feat: restart a game on popup closing

--- hangman-game/src/components/popup/popup.component.tsx
+++ hangman-game/src/components/popup/popup.component.tsx
@@ -1,11 +1,11 @@
 import "./popup.css";
 import { IPopupProps } from "./popup.types";
 
-export const Popup = ({hasWon, answer, closePopup, restartGame }: IPopupProps) => {
+export const Popup = ({hasWon, answer, restartGame }: IPopupProps) => {
   return (
-    <div onClick={closePopup} className="popup-container">
+    <div onClick={restartGame} className="popup-container">
       <div className="popup-wrapper">
-        <div  onClick={closePopup} className="popup-cross">x</div>
+        <div onClick={restartGame} className="popup-cross">x</div>
         <div className="popup">
           <div className="popup__result">
             {hasWon ? "You have won!" : "You have lost..."}
--- hangman-game/src/components/popup/popup.types.ts
+++ hangman-game/src/components/popup/popup.types.ts
@@ -3,6 +3,6 @@ import { MouseEvent } from "react";
 export interface IPopupProps {
     hasWon: boolean;
     answer: string;
-    closePopup: (e: MouseEvent) => void;
+    closePopup?: (e: MouseEvent) => void;
     restartGame?: () => void;
 }
